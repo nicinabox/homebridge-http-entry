a1de2a0cf3527b0b418de928816201d444fa9922
Nic Haynes <nic@nicinabox.com>
2021-12-14 01:14:19 -0600

    Disable debug logger setting

--- src/lib/configureLogger.ts
+++ src/lib/configureLogger.ts
@@ -1,6 +1,6 @@
-import { Logger } from 'homebridge/lib/logger';
+// import { Logger } from 'homebridge/lib/logger';
 
 export default (log, enableDebug = true) => {
-    Logger.setDebugEnabled(enableDebug);
+    // Logger.setDebugEnabled(enableDebug);
     return log;
 };
